8ac6ffeb6902ed4a1779cd1e22d43c3b8a066ef5
Chameli <chame@courera.com>
2020-09-26 21:58:35 +0530

    Initial commit

---
+++ src/main/java/com/example/sampathds/SampathdsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sampathds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampathdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampathdsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sampathds/SampathdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sampathds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampathdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
