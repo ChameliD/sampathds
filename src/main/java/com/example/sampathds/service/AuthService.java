caa4bbe50c494cb31ce9146c0df85c0c3bb0bec1
Chameli <chame@courera.com>
2020-09-29 11:58:25 +0530

    encrypt password

--- src/main/java/com/example/sampathds/service/AuthService.java
+++ src/main/java/com/example/sampathds/service/AuthService.java
@@ -6,6 +6,7 @@ import com.example.sampathds.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,14 +14,22 @@ public class AuthService
 {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public ResponseEntity signup(RegisterRequest registerRequest)
     {
         User user=new User();
         user.setUserName(registerRequest.getUserName());
-        user.setPassword(registerRequest.getPasssword());
         user.setEmail(registerRequest.getEmail());
+        user.setPassword(encodePassword(registerRequest.getPasssword()));
+
         userRepository.save(user);
         return new ResponseEntity(HttpStatus.OK);
 
+    }
+    private String encodePassword(String password)
+    {
+        return passwordEncoder.encode(password);
     }
 }
